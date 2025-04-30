7d937e396fbeb3dced4f050230dcec1753e74718
FetoyuDev <jorgelucasgamer4792@gmail.com>
2025-04-29 22:05:04 -0400

    update init.py

--- init.py
+++ init.py
@@ -1,11 +1,13 @@
-import os
+import subprocess
 
-user = os.system("whoami")
-hostname = os.system("hostname")
-home = os.system("$XDG_CONFIG_HOME")
-active_folder = os.system('echo "$(pwd)"')
-date = os.system("date")
+# Captura os valores dos comandos
+user = subprocess.getoutput("whoami")
+hostname = subprocess.getoutput("hostname")
+home = subprocess.getoutput('echo $XDG_CONFIG_HOME')
+active_folder = subprocess.getoutput('pwd')
+date = subprocess.getoutput("date")
 
+# Combina os valores em uma lista
 all_data = [user, hostname, home, active_folder, date]
 
 print(f"Todos os dados: {all_data}")
